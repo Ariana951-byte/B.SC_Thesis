66d831095490842efe2eee7d29b172735361f409
Ariana951-byte <sakura.akhter1234@gmail.com>
2026-04-25 01:42:34 -0400

    Update main.m

--- matlab/main.m
+++ matlab/main.m
@@ -1,1 +1,44 @@
 
+clc; clear;
+
+% Objective function coefficients
+C = [20 18 3 10];   % [W X Y Z]
+
+% Constraints (A * x <= b)
+A = [
+    15 15 3.75 3;
+    5 8 4 5;
+    6 1 2 5
+];
+
+b = [30; 40; 10];
+
+n = length(C);
+
+% Generate binary solution space
+binary_space = generate_binary_space(n);
+
+best_val = -inf;
+best_sol = [];
+
+for i = 1:size(binary_space,1)
+    
+    x = binary_space(i,:);
+    
+    % Check constraints
+    if all(A * x' <= b)
+        
+        val = evaluate_objective(C, x);
+        
+        if val > best_val
+            best_val = val;
+            best_sol = x;
+        end
+    end
+end
+
+disp('Best Solution:')
+disp(best_sol)
+
+disp('Maximum Value:')
+disp(best_val)
